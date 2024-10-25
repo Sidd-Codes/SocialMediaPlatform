c827ea207c667d9cb075130f396d57684f536cf2
Sidd-Codes <107668103+Sidd-Codes@users.noreply.github.com>
2024-10-25 00:01:23 -0400

    Update populate.js

--- populate.js
+++ populate.js
@@ -15,7 +15,7 @@ const CSVToJSON = require("csvtojson");
 
 //Input Files
 const actor_inputFile = './input/actors.csv';
-const posts_inputFile = './input/posts.csv';
+const posts_inputFile = './input/MergedTweets.csv';
 //const replies_inputFile = './input/replies.csv';
 const notifications_inputFile = './input/notifications (read, like).csv';
 const notifications_replies_inputFile = './input/notifications (reply).csv';
